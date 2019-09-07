39ce345f8f2b248cb2749b5bc8212904a6315031
David Mullins <Mullins6323@gmail.com>
2019-09-07 13:53:43 -0500

    Add project files.

---
+++ MyGarage/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyGarage
+{
+    abstract class Vehicle
+    {
+        public int FuelCapacity { get; set; }
+        public CarColor CarColor { get; set; }
+        public int PassengerOccupancy { get; set; }
+
+        public void Refueling()
+        {
+            Console.WriteLine("Refueling");
+        }
+
+        public abstract void Driving();
+
+    }
+
+    enum CarColor
+    {
+        red,
+        black,
+        blue,
+        white,
+        orange,
+    }
+
+}
